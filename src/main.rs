a07fb387c695c5e91c38c6d78dbb8aabe0ec8531
karl <elrion018@gmail.com>
2022-02-06 17:10:58 +0900

    feat : 초기 단어 설정 및 단어 입력 로직 추가

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,27 @@
+use std::io::stdin;
+
 fn main() {
-    println!("Hello, world!");
+    // 초기 단어를 설정한다. 일단 Hello로 설정
+    let selected_word = String::from("Hello");
+
+    // 사용자가 단어를 입력하게끔 한다.
+    loop {
+        let mut guess_word = String::new();
+        stdin().read_line(&mut guess_word);
+        guess_word = guess_word.trim().to_string();
+        match guess_word.len() {
+            1..=4 => {
+                println!("더 긴 단어를 입력해주세요.");
+                continue;
+            }
+            5 => {
+                println!("good");
+                break;
+            }
+            _ => {
+                println!("더 짧은 단어를 입력해주세요.");
+                continue;
+            }
+        }
+    }
 }
