5f549bcf528b4d2c89b63ccd931394c0c7f351da
karl <elrion018@gmail.com>
2022-02-08 23:29:34 +0900

    feat : 완성

--- src/main.rs
+++ src/main.rs
@@ -5,25 +5,26 @@ use system::System;
 
 fn main() {
     let mut game_system = System::new();
-    game_system.random_selected_word();
+    game_system.random_target_word();
 
     // 사용자가 단어를 입력하게끔 한다.
     loop {
         let mut guess_word = String::new();
+        game_system.display_words();
         stdin().read_line(&mut guess_word);
         guess_word = guess_word.trim().to_string();
         match guess_word.len() {
-            1..=4 => {
-                println!("더 긴 단어를 입력해주세요.");
-                continue;
-            }
             5 => {
                 game_system.update_current_word(guess_word);
+                game_system.display_words();
             }
             _ => {
-                println!("더 짧은 단어를 입력해주세요.");
                 continue;
             }
+        }
+
+        if game_system.over() {
+            break;
         }
     }
 }
--- src/system/mod.rs
+++ src/system/mod.rs
@@ -1,31 +1,32 @@
 mod words;
+use colored::*;
 use rand::prelude::*;
 use words::valid_words;
 
+pub type CorrectLetter = Option<bool>;
+
 pub struct System {
-    selected_word: String,
+    target_word: String,
     current_word: String,
-    past_words: Vec<String>,
-    valid_words: Vec<String>,
+    past_words: [Option<String>; 5],
 }
 
 impl System {
     // 생성자
     pub fn new() -> System {
         System {
-            selected_word: String::from(""),
+            target_word: String::from(""),
             current_word: String::from(""),
-            past_words: Vec::new(),
-            valid_words: vec![],
+            past_words: [None, None, None, None, None],
         }
     }
 
     // 랜덤으로 selected word를 고르는 메소드
-    pub fn random_selected_word(&mut self) -> () {
-        self.selected_word =
+    pub fn random_target_word(&mut self) -> () {
+        self.target_word =
             String::from(words::valid_words[thread_rng().gen_range(0..words::valid_words.len())]);
 
-        println!("{}", self.selected_word);
+        println!("{}", self.target_word);
     }
 
     // current_word를 update하는 메소드
@@ -34,13 +35,8 @@ impl System {
             true => {
                 self.add_past_word();
                 self.current_word = input_word;
-
-                println!("{}", self.current_word);
-                println!("{:?}", self.past_words);
             }
-            _ => {
-                println!("유효한 알파벳으로 구성된 단어를 입력해주세요.")
-            }
+            _ => (),
         }
     }
 
@@ -57,13 +53,99 @@ impl System {
         }
 
         // 유효한 단어 목록에 포함되어있는 지 확인
-        return self.valid_words.contains(input_word);
+        return words::valid_words.contains(&input_word.as_str());
     }
 
     // current_word를 past_words에 add 하는 메소드
     pub fn add_past_word(&mut self) -> () {
-        if self.current_word.len() != 0 {
-            self.past_words.push(self.current_word.clone())
+        let first_none = self.past_words.iter_mut().find(|x| x.is_none());
+
+        if let Some(item) = first_none {
+            if self.current_word.len() != 0 {
+                item.replace(self.current_word.clone());
+            }
         }
+    }
+    pub fn check_by_target_word(&self, check_word: &String) -> [CorrectLetter; 5] {
+        let mut result = [Some(true); 5];
+
+        for (i, (target_word_char, check_word_char)) in
+            self.target_word.chars().zip(check_word.chars()).enumerate()
+        {
+            result[i] = if target_word_char == check_word_char {
+                Some(true)
+            } else if self.target_word.chars().any(|c| c == check_word_char) {
+                Some(false)
+            } else {
+                None
+            }
+        }
+
+        result
+    }
+
+    pub fn format_with_color(
+        &self,
+        word: &String,
+        format: [CorrectLetter; 5],
+    ) -> Vec<ColoredString> {
+        word.chars()
+            .enumerate()
+            .map(|(i, c)| match format[i] {
+                Some(true) => c.to_string().green(),
+                Some(false) => c.to_string().yellow(),
+                None => c.to_string().normal(),
+            })
+            .collect()
+    }
+
+    pub fn display_words(&mut self) -> () {
+        print!("\x1B[2J");
+        print!("\x1B[0;0f");
+
+        println!("┏━━━━━━━━━━━━━━━━━┓");
+        println!("┃   W O R D L E   ┃");
+        println!("┣━━━━━━━━━━━━━━━━━┫");
+
+        let mut lines = 0;
+        for past_word in self.past_words.iter() {
+            if let Some(word) = past_word {
+                print!("┃    ");
+                let formatted_words = self.format_with_color(word, self.check_by_target_word(word));
+                formatted_words.iter().for_each(|c| print!("{} ", c));
+                println!("   ┃");
+                lines += 1
+            } else {
+                break;
+            }
+        }
+
+        if self.current_word.len() != 0 {
+            print!("┃    ");
+            let formatted_words = self.format_with_color(
+                &self.current_word,
+                self.check_by_target_word(&self.current_word),
+            );
+            formatted_words.iter().for_each(|c| print!("{} ", c));
+            println!("   ┃");
+            lines += 1;
+        }
+
+        for _ in lines..self.past_words.len() + 1 {
+            println!("┃                 ┃");
+        }
+        println!("┗━━━━━━━━━━━━━━━━━┛");
+    }
+
+    pub fn win(&self) -> bool {
+        self.current_word == self.target_word
+    }
+
+    pub fn lose(&self) -> bool {
+        !self.win() && self.past_words.iter().all(|x| x.is_some())
+    }
+
+    pub fn over(&self) -> bool {
+        self.win() || self.lose()
     }
 }
